3945c7aec9eaa1623e0ba1cf19c9700d01ac1e95
Vojtech Pacik <vojtech.pacik@gcern.ch>
2017-07-14 16:53:01 +0200

    Minimal components added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='aligem',
+      version='0.1',
+      description='ALICE Grid Enviroment Manager',
+      url='http://github.com/vpacik/aligem',
+      author='Vojtech Pacik',
+      author_email='vojtech.pacik@cern.ch',
+      license='APACHE',
+      packages=['aligem'],
+      zip_safe=False)
