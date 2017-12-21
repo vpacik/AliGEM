bbc508af1a1218d7b29a3a91af67b15af7c56326
Vojtech Pacik <vojtech.pacik@gcern.ch>
2017-12-21 17:15:26 +0100

    Minimalistic implementation modifed for alpha testing of packaging and command line tools

--- aligem/tests/test_aligem.py
+++ aligem/tests/test_aligem.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import aligem
 
-class TestJoke(TestCase):
-    def test_is_string(self):
-        s = aligem.test.hi()
-        self.assertTrue(isinstance(s, basestring))
+# class TestJoke(TestCase):
+    # def test_is_string(self):
+        # s = aligem.command_line.hi()
+        # self.assertTrue(isinstance(s, basestring))
