7db869323ae0017960df8727e9616c3fdeb4454d
Vojtech Pacik <vojtech.pacik@gcern.ch>
2018-01-22 17:46:43 +0100

    -u flag moved to jobs status subparser (not used elsewhere, except when whoami() does not work)

--- aligem/command_line.py
+++ aligem/command_line.py
@@ -8,7 +8,6 @@ def main() :
 
     ### top-level group (L0)
     parser = argparse.ArgumentParser(description="Welcome to AliGEM - ALICE Grid Enviroment Manager - toolbox for handling Grid related operations.")
-    parser.add_argument("-u","--user", help="specify USER as CERN username", default=local_user)
     parser.add_argument("-v","--verbose", help="produce verbose output", action="store_true")
     parser.add_argument("-d","--debug", help="debugging mode (additional printout)", action="store_true")
     parser.add_argument("--version", action="version", version='0.1', help="print current version")
@@ -20,12 +19,13 @@ def main() :
     jobs_subparsers = parser_jobs.add_subparsers(dest="job_command")
 
     # jobs sub-parsers (L2)
-    jobs_subparser_status = jobs_subparsers.add_parser("status", help = "print overview of current grid jobs")
+    jobs_subparser_status = jobs_subparsers.add_parser("status", help = "print overview of currently registered grid jobs")
     # jobs_subparser_status.add_argument("-u","--user", help="specify USER as CERN username")
     jobs_subparser_status.add_argument("-o","--offline", help="running in OFFLINE mode for testing purposes", action="store_true", default=False)
     jobs_subparser_status.add_argument("--only-positive", help="print only states with at least 1 (sub)job", action="store_true", default=False)
+    jobs_subparser_status.add_argument("-u","--user", help="specify USER as CERN username", default=local_user)
 
-    jobs_subparser_kill = jobs_subparsers.add_parser("kill", help = "kill grid job(s)")
+    jobs_subparser_kill = jobs_subparsers.add_parser("kill", help = "kill grid job(s) in DONE state")
     # jobs_subparser_kill.add_argument("-u","--user", help="specify USER as CERN username")
 
     jobs_subparser_resubmit = jobs_subparsers.add_parser("resub", help = "re-submit all grid job(s) in ERROR, EXPIRED or ZOMBIE state")
