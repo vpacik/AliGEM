15d1aece3a2c9b57f57dd97b3f1b68f4ab692592
Vojtech Pacik <vojtech.pacik@gcern.ch>
2018-02-07 11:45:50 +0100

    Basic token operations impl'd

---
+++ aligem/token.py
@@ -0,0 +1,44 @@
+# AliEn token operations handler
+
+import jobs
+
+def main() :
+    print "Token.main() says hi"
+
+def init(debug=False, verbose=False) :
+    if debug : print "init inside"
+    output = jobs.exec_alien_cmd('alien-token-init',verbose)
+
+    if output['returncode'] == 0 :
+        print "Token succesfully initialized!"
+    else :
+        print "Token NOT initialized!"
+        if verbose :
+            print output['output'].strip()
+
+def info(debug=False, verbose=False) :
+    if debug : print "token.info()"
+    output = jobs.exec_alien_cmd('alien-token-info',verbose)
+    print output['output'].strip()
+
+def destroy(debug=False,verbose=False) :
+    if debug : print "destroy inside"
+    output = jobs.exec_alien_cmd('alien-token-destroy',verbose)
+
+    if output['returncode'] == 0 :
+        print "Token succesfully destroyed!"
+    else :
+        print "Token not destoyed"
+        if verbose :
+            print output['output'].strip()
+
+def check(debug=False,verbose=False) :
+    token = jobs.exec_alien_cmd('alien-token-info',verbose)
+
+    if token['returncode'] == 1 :
+        # not succesfull
+        if verbose : print "Token not validated!"
+        if debug : print token
+        return False
+
+    return True
